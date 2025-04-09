66a9808419e993ead1149874c4dae8260b1b81cd
youssef483 <youssef5285962ali@gmail.com>
2025-04-09 17:05:42 +0200

    Second task

---
+++ session2.php
@@ -0,0 +1,40 @@
+<?php
+
+// 1
+
+$num1 = 5;
+
+$num2 = 10;
+
+$total = $num1 + $num2;
+
+echo $total . "<br>";
+
+
+// 2
+
+$a = 4;
+
+++$a;
+
+echo "I have $a books <br>";
+
+$a = 4;
+
+$a++;
+
+echo "I have $a books <br>";
+
+// 3
+
+$first = "In";
+
+$second = ' PHP';
+
+$third = " Course";
+
+echo $first . $second . $third;
+
+?>
+
+
